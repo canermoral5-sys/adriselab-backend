dc440fcd8ab118653d574840e440e8440136d705
canermoral5-sys <canermoral5@gmail.com>
2025-08-30 00:10:06 +0300

    Update authorize.js

--- api/meta/authorize.js
+++ api/meta/authorize.js
@@ -1,9 +1,17 @@
+// /api/meta/authorize.js
 const FB_OAUTH_URL = "https://www.facebook.com/v20.0/dialog/oauth";
 
 export default async function handler(req, res) {
   const { META_APP_ID, META_REDIRECT_URI } = process.env;
-  const scope = ["ads_read","ads_management"].join(",");
-  const url = `${FB_OAUTH_URL}?client_id=${META_APP_ID}&redirect_uri=${encodeURIComponent(META_REDIRECT_URI)}&response_type=code&scope=${scope}`;
+  const scope = "ads_read,ads_management"; // test için istersen sadece ads_read
+
+  const url =
+    `${FB_OAUTH_URL}` +
+    `?client_id=${encodeURIComponent(META_APP_ID)}` +
+    `&redirect_uri=${encodeURIComponent(META_REDIRECT_URI)}` +
+    `&response_type=code` +
+    `&scope=${encodeURIComponent(scope)}`;
+
   res.writeHead(302, { Location: url });
   res.end();
 }
